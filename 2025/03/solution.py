b8e1220497289d29ce6a0c7cd31abcc1e2c9ab7c
MathiasDPX <mathias@dupeux.net>
2025-12-21 13:48:25 +0100

    unify solution for 2025/03

--- 2025/03/solution.py
+++ 2025/03/solution.py
@@ -1,27 +1,41 @@
 lines = []
 
-lines = open('input.txt', 'r').read().splitlines(False)
+lines = open("input.txt", "r").read().splitlines(False)
 
-digits = 12 # 2 for first part
-result = 0
 
-for line in lines:
-    length = len(line)
-    selected = []
-    start = 0
-    
-    for i in range(digits):
-        end = length - (digits - 1 - i)
-        best_idx = start
-        
-        for j in range(start, end):
-            if line[j] > line[best_idx]:
-                best_idx = j
-        
-        selected.append(line[best_idx])
-        start = best_idx + 1
-        
-    top = int(''.join(selected))
-    result += top
-    
-print("Solution:", result)
+def solve(digits):
+    result = 0
+
+    for line in lines:
+        length = len(line)
+        selected = []
+        start = 0
+
+        for i in range(digits):
+            end = length - (digits - 1 - i)
+            best_idx = start
+
+            for j in range(start, end):
+                if line[j] > line[best_idx]:
+                    best_idx = j
+
+            selected.append(line[best_idx])
+            start = best_idx + 1
+
+        top = int("".join(selected))
+        result += top
+
+    return result
+
+
+def part1():
+    return solve(2)
+
+
+def part2():
+    return solve(12)
+
+
+if __name__ == "__main__":
+    print(f"Solution part 1: {part1()}")
+    print(f"Solution part 2: {part2()}")
