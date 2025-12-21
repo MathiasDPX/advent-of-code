dee7a5e744c1df1ab8f12db8db4cdabf29d684b5
MathiasDPX <mathias@dupeux.net>
2025-12-21 13:46:27 +0100

    unify solution for 2025/01

--- 2025/01/solution.py
+++ 2025/01/solution.py
@@ -3,30 +3,51 @@ lines = []
 with open("input.txt", "r") as f:
     lines = f.readlines()
 
-idx = 50
 
-solution1 = 0
-solution2 = 0
+def part1():
+    idx = 50
+    solution = 0
 
-for line in lines:
-    move = line[1:].strip()
-    move = int(move)
+    for line in lines:
+        move = line[1:].strip()
+        move = int(move)
 
-    step = 1
-    if line.startswith("L"):
-        step = -1
-        
-    while move != 0:
-        idx += step
-        move -= 1
+        if line.startswith("L"):
+            move *= -1
 
-        idx = idx % 100
+        idx += move
+        idx %= 100
 
         if idx == 0:
-            solution2 += 1
-    
-    if idx == 0:
-            solution1 += 1
+            solution += 1
 
-print(f"Solution part 1: {solution1}")
-print(f"Solution part 2: {solution2}")
+    return solution
+
+
+def part2():
+    idx = 50
+    solution = 0
+
+    for line in lines:
+        move = line[1:].strip()
+        move = int(move)
+
+        step = 1
+        if line.startswith("L"):
+            step = -1
+
+        while move != 0:
+            idx += step
+            move -= 1
+
+            idx = idx % 100
+
+            if idx == 0:
+                solution += 1
+
+    return solution
+
+
+if __name__ == "__main__":
+    print(f"Solution part 1: {part1()}")
+    print(f"Solution part 2: {part2()}")
